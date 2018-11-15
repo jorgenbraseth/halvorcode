fe49d26759695f2477ece2ebec21662730e0bc10
jorgenbraseth <jorgen.braseth@unacast.com>
2018-11-15 12:32:14 +0100

    cleaned it up a bit

--- code.js
+++ code.js
@@ -1,16 +1,9 @@
 const canvas = document.getElementById("screen");
 const screen = canvas.getContext("2d");
 
-let xPos = 0;
-let yPos = 0;
-let disco = false;
-screen.fillStyle = "#bdba1d";
-
-
-function makeSquare(x,y) {
+function makeShape(x, y) {
     return {
-        x: x,
-        y: y,
+        x, y,
         size: 5+Math.random()*20,
         xSpeed: 5-Math.random()*10,
         ySpeed: -10 - Math.random()*10,
@@ -27,7 +20,6 @@ function draw() {
         s.y += s.ySpeed;
         s.x += s.xSpeed;
         s.ySpeed += 1;
-        // screen.fillRect(s.x-25,s.y-25,50,50);
         screen.beginPath();
         screen.arc(s.x,s.y,s.size,0,2*Math.PI);
         screen.fill();
@@ -44,7 +36,7 @@ function draw() {
 
 canvas.onmousemove = (evt) => {
     squares.push(
-        makeSquare(evt.offsetX, evt.offsetY)
+        makeShape(evt.offsetX, evt.offsetY)
     )
 };
 draw();
